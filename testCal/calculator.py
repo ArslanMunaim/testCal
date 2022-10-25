8bf7edf6844b1a77b5d727f16bd262c62220da6b
ArslanMunaim <arslanmunim@gmail.com>
2022-10-25 23:05:35 +0200

    Update Calculator.py

--- testCal/calculator.py
+++ testCal/calculator.py
@@ -28,35 +28,7 @@ if __name__ == '__main__':
     print('Hello! GITHUB Calculator')
 
 
-#import pytest
-import pytest
 
-from calculator import add_num
-from calculator import sub_num
-from calculator import mul_num
-from calculator import div_num
-
-def test_add_positives():
-    assert add_num(2,3) == 5
-
-
-def test_add_zero():
-    assert add_num(2, 0) == 2
-
-
-def test_add_negative():
-    assert add_num(2, -3) ==  -1
-
-
-def test_string_expect_exception_firstinput():
-    with pytest.raises(TypeError):
-        add_num('Random Text' , 4)
-        
-        
-def test_string_expect_exception_secondinput():
-    with pytest.raises(TypeError):
-        add_num(4,'Random Text')
-        
         
 
         
