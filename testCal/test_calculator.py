86445909ddea78bf25356ed66343124de92c63b8
ArslanMunaim <arslanmunim@gmail.com>
2022-10-25 22:53:49 +0200

    Testing Functionality: add,sub,mul

--- testCal/test_calculator.py
+++ testCal/test_calculator.py
@@ -18,14 +18,67 @@ def test_add_negative():
     assert add_num(2, -3) ==  -1
 
 
-def test_string_expect_exception_firstinput():
+def test_string_expect_exception_firstinputAdd():
     with pytest.raises(TypeError):
         add_num('Random Text' , 4)
         
         
-def test_string_expect_exception_secondinput():
+def test_string_expect_exception_secondinputAdd():
     with pytest.raises(TypeError):
         add_num(4,'Random Text')
+    
+
+def test_add_pushing_limits():
+    assert add_num(9999999999, 9999999999) ==  19999999998
+
+    
+  
+def test_sub_positives():
+    assert sub_num (3,2) == 1
+    
+
+def test_sub_zero_1():
+    assert sub_num (3,0) == 3
+    
+
+def test_sub_zero_2():
+    assert sub_num (0,3) == -3
+ 
+ 
+def test_string_expect_exception_firstinputSub():
+    with pytest.raises(TypeError):
+        sub_num('Random Text' , 4)
+
+
+def test_string_expect_exception_secondinputSub():
+    with pytest.raises(TypeError):
+        sub_num(4,'Random Text')
+        
+    
+def test_mul_positives():
+    assert mul_num(2,2) == 4
+    
+ 
+def test_mul_zero():
+    assert mul_num(2, 0) == 0
+
+
+def test_mul_negative():
+    assert mul_num(2, -3) ==  -6
+
+
+def test_string_expect_exception_firstinputMUL():
+    with pytest.raises(TypeError):
+        mul_num('Random Text' , 4)
+
+
+def test_string_expect_exception_secondinputMUL():
+    with pytest.raises(TypeError):
+        mul_num(4,'Random Text')
+    
+
+
+    
 
 
 
