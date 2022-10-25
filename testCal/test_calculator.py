2f64cb335754f7a92a386e619b55bf0a427f7788
ArslanMunaim <arslanmunim@gmail.com>
2022-10-25 23:11:13 +0200

    Testing Functionality: add,sub,mul,div

--- testCal/test_calculator.py
+++ testCal/test_calculator.py
@@ -18,12 +18,12 @@ def test_add_negative():
     assert add_num(2, -3) ==  -1
 
 
-def test_string_expect_exception_firstinputAdd():
+def test_string_expect_exception_firstinput_Add():
     with pytest.raises(TypeError):
         add_num('Random Text' , 4)
         
         
-def test_string_expect_exception_secondinputAdd():
+def test_string_expect_exception_secondinput_Add():
     with pytest.raises(TypeError):
         add_num(4,'Random Text')
     
@@ -45,12 +45,12 @@ def test_sub_zero_2():
     assert sub_num (0,3) == -3
  
  
-def test_string_expect_exception_firstinputSub():
+def test_string_expect_exception_firstinput_Sub():
     with pytest.raises(TypeError):
         sub_num('Random Text' , 4)
 
 
-def test_string_expect_exception_secondinputSub():
+def test_string_expect_exception_secondinput_Sub():
     with pytest.raises(TypeError):
         sub_num(4,'Random Text')
         
@@ -67,7 +67,27 @@ def test_mul_negative():
     assert mul_num(2, -3) ==  -6
 
 
+def test_div_positives():
+    assert mul_num(2,2) == 1
+    
+ 
+def test_div_negative():
+    assert mul_num(2, -3) ==  -6
 
+
+def test_div_zero():
+    with pytest.raises(ZeroDivisionError)
+        div_num(314,0)
+
+
+def test_string_expect_exception_firstinput_DIV():
+    with pytest.raises(TypeError):
+        div_num('Random Text' , 4)
+
+
+def test_string_expect_exception_secondinput_DIV():
+    with pytest.raises(TypeError):
+        div_num(4,'Random Text')
 
     
 
